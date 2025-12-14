82e9e7193bd446f01a878249ec6ebd01436deffe
roger1ms <olegtatur228@mail.ru>
2025-12-14 23:14:15 +0700

    Delete excess folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/olegt/Desktop/nsu-oop-labs/lab1
-# Build directory: C:/Users/olegt/Desktop/nsu-oop-labs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/olegt/Desktop/nsu-oop-labs/build/BigInt_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/run_tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/olegt/Desktop/nsu-oop-labs/build/run_tests[1]_tests.cmake")
-  include("C:/Users/olegt/Desktop/nsu-oop-labs/build/run_tests[1]_tests.cmake")
-else()
-  add_test(run_tests_NOT_BUILT run_tests_NOT_BUILT)
-endif()
